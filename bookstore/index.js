46851d730da6c3a772579decbe1842cdfc1b8a50
clinturbin <clinturbin@gmail.com>
2018-09-11 16:17:08 -0400

    Added buttons for deleting and updating title for blog posts

--- bookstore/index.js
+++ bookstore/index.js
@@ -1,69 +1,7 @@
-// Description of what we want the page to look like in its entirety
-// It doesn't actually create it
-// This is what we are responsible for in React
-
-// let page = {
-//     tag: 'div',
-//     children: [
-//         {
-//             element: 'h1', 
-//             children: ['Bookstore'],
-//             props: {
-//                 className: 'big-header'
-//             }
-//         },
-//         {
-//             element: 'ul', children: [
-//                 {element: 'li', children: ['Book 1']},
-//                 {element: 'li', children: ['Book 2']},
-//                 {element: 'li', children: ['Book 3']},
-//             ]
-//         },
-//         {
-//             element: 'footer', children: 'Copyright 2018'
-//         },
-//         {
-//             element: 'a', 
-//             children: ['My Website'],
-//             props: {
-//                 href: 'mypage.com'
-//             }
-//         }
-//     ] 
-// }
-
-//----------------------------
-
-// let createElement = (element, props, children) => {
-//     return {
-//         element: element,
-//         children: children,
-//         props: props
-//     }
-// }
-
-// let createElement = (element, props, children) => {
-//     return {element, children, props}
-// };
-
-// let vdom = h('div', {}, [
-//     h('h1', {className: 'big-header'}, ['Bookstore']),
-//     h('ul', {}, [
-//         h('li', {}, ['Book1']),
-//         h('li', {}, ['Book2']),
-//         h('li', {}, ['Book3']),
-//     ]),
-//     h('footer', {}, ['Copyright 2018']),
-//     h('a', {href: 'mypage.com'}, ['My Website']),
-// ]);
-
-// ReactDOM.render(vdom, document.querySelector('.react-root'));
-
 const h = React.createElement;
 
-
 // --------STATE----------------
-// The only things that wil change
+// The only things that will change
 let books = [
     'A Tale of Two Cities',
     'Book2',
@@ -82,13 +20,7 @@ let removeBook = (bookToRemove) => {
 };
 
 let addToBookTitle = (bookToChange) => {
-    books = books.map(book => {
-        if (book === bookToChange) {
-            return book + 's'
-        } else {
-            return book
-        }
-    });
+    books = books.map(book => book === bookToChange ? book + 's' : book)
 };
 
 let BookRow = (props) => 
--- react-blog/index.js
+++ react-blog/index.js
@@ -2,60 +2,73 @@ let posts = [
     {
         "userId": 1,
         "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "title": "Post1",
         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
       },
       {
         "userId": 1,
         "id": 2,
-        "title": "qui est esse",
+        "title": "Post2",
         "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
       },
       {
         "userId": 1,
         "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "title": "Post3",
         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
       },
       {
         "userId": 1,
         "id": 4,
-        "title": "eum et est occaecati",
+        "title": "Post4",
         "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
       },
       {
         "userId": 1,
         "id": 5,
-        "title": "nesciunt quas odio",
+        "title": "Post5",
         "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
       },
       {
         "userId": 1,
         "id": 6,
-        "title": "dolorem eum magni eos aperiam quia",
+        "title": "Post6",
         "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
       },
       {
         "userId": 1,
         "id": 7,
-        "title": "magnam facilis autem",
+        "title": "Post7",
         "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
       },
       {
         "userId": 1,
         "id": 8,
-        "title": "dolorem dolore est ipsam",
+        "title": "Post8",
         "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
       },
       {
         "userId": 1,
         "id": 9,
-        "title": "nesciunt iure omnis dolorem tempora et accusantium",
+        "title": "Post9",
         "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
       }
 ];
 
 let h = React.createElement;
+
+let removePost = (postToRemove) => {
+  posts = posts.filter(post => post.id !== postToRemove.id);
+};
+
+let snakeify = (blogPost) => {
+  posts = posts.map(post => 
+    post.id === blogPost.id ? 
+      Object.assign({}, post, {title: post.title + 's'})
+    :
+      post
+    )
+};
 
 let PageHeader = (props) => 
   h('h1', {className: 'big-header'}, ['React Blog'])
@@ -63,6 +76,18 @@ let PageHeader = (props) =>
 let BlogRow = (props) => 
   h('li', {}, [
     h('h2', {}, props.title),
+    h('button', {
+      onClick: () => {
+        removePost(props);
+        rerender();
+      }
+    }, 'Remove Post'),
+    h('button', {
+      onClick: () => {
+        snakeify(props);
+        rerender();
+      } 
+    }, 'Snakefiy'),
     h('p', {}, props.body)
   ])
 
@@ -86,4 +111,9 @@ let BlogPage = (props) =>
   ])
 
 
-ReactDOM.render(h(BlogPage, { posts }), document.querySelector('.react-root'));
+let rerender = () => {
+  ReactDOM.render(h(BlogPage, { posts }), document.querySelector('.react-root'));
+};
+
+rerender();
+
