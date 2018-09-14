0da61ae2d0f84aafa49bfd4d18b963a1cbb01674
clinturbin <clinturbin@gmail.com>
2018-09-14 10:39:31 -0400

    Removed reference comments

--- wassup/index.js
+++ wassup/index.js
@@ -1,8 +1,5 @@
-const h = React.createElement;
-
 let generateId = () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
 
-// props = { addWassup: addWassup }
 class WassupInputForm extends React.Component {
     constructor(props) {
         super(props);
@@ -19,7 +16,8 @@ class WassupInputForm extends React.Component {
                         event.preventDefault();
                         this.props.addWassup(this.state.newWassup);
                     }
-            }>
+                }
+            >
                 <input 
                     type='text'
                     value={this.state.newWassup}
@@ -33,31 +31,10 @@ class WassupInputForm extends React.Component {
                 <input type='submit' value='Post'/>
             </form>
         );
-
-        // return h('form', {
-        //     onSubmit: (event) => {
-        //         event.preventDefault();
-        //         // Add the current input box content to the array in key wassups in HomePage
-        //         // class. props is passed from HomePage which is where addWassup is called from
-        //         this.props.addWassup(this.state.newWassup);
-        //     }
-        // },
-            
-        //     h('input', {
-        //         type: 'text',
-        //         value: this.state.newWassup, // gets the current content from text input
-        //         onChange: (event) => {
-        //             let value = event.target.value; // sets value to current content
-        //             this.setState( { newWassup: value }) // set the state of newWassup to current content
-        //         }
-        //     }),
-        //     h('input', { type: 'submit', value: 'Post'})
-        // )
     }
 };
 
 let WassupRow = (props) => 
-    // h('li', {}, props.wassup.content)
     <li>
         <p>{props.wassup.content}</p>
     </li>
@@ -68,11 +45,6 @@ let WassupList = (props) =>
             <WassupRow wassup={wassup} key={wassup.id} />
         ).reverse() }
     </ul>
-    // h('ul', {}, 
-    //     props.wassups.map(wassup => 
-    //         h(WassupRow, {wassup: wassup, key: wassup.id})
-    //     ).reverse()
-    // )
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -100,15 +72,7 @@ class HomePage extends React.Component {
                 <WassupList wassups={this.state.wassups} />
             </div>
         );
-        // return h('div', {}, 
-        //     h('h1', {}, 'Wassup!!!!'),
-        //     h(WassupInputForm, {addWassup: addWassup}),
-        //     h(WassupList, {
-        //         wassups: this.state.wassups
-        //     })
-        // );
     };
 };
 
-// ReactDOM.render(h(HomePage), document.querySelector('.react-root'));
 ReactDOM.render(<HomePage/>, document.querySelector('.react-root'));
